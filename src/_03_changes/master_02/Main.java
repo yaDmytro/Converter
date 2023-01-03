9c013f75a8b1414cea3c8af9da569a71c54d11ae
Dmytro Yakovenko <dima.iakovenko@yahoo.com>
2023-01-03 18:32:33 +0200

    add converter for Mile into Kilometers

--- src/_03_changes/master_02/Main.java
+++ src/_03_changes/master_02/Main.java
@@ -1,4 +1,19 @@
 package _03_changes.master_02;
+import java.lang.*;
 
 public class Main {
+
+
+    public static double mileConverterToKilometers(double sizeInMiles){
+        return sizeInMiles * 1.609344;
+    }
+
+    public static void main(String[] args){
+
+        double tenMiles = 10;
+        double tenMilesInKilometers = mileConverterToKilometers(tenMiles);
+
+        System.out.printf("%f miles in kilometers will be %f.", tenMiles, tenMilesInKilometers);
+    }
 }
+
